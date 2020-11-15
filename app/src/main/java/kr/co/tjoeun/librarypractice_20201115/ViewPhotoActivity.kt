b78fe40e6e83a518855fd55c31c9c89f05b189a9
say-102 <say70007@naver.com>
2020-11-15 11:41:33 +0900

    사진 크게보기 기능 추가중

--- app/src/main/java/kr/co/tjoeun/librarypractice_20201115/ViewPhotoActivity.kt
+++ app/src/main/java/kr/co/tjoeun/librarypractice_20201115/ViewPhotoActivity.kt
@@ -1,29 +1,24 @@
 package kr.co.tjoeun.librarypractice_20201115
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 import kr.co.tjoeun.lifecycle_20201114.BaseActivity
 
-class MainActivity : BaseActivity() {
+class ViewPhotoActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_view_photo)
         setupEvents()
         setValues()
     }
 
     override fun setupEvents() {
 
-        viewPhotoBtn.setOnClickListener {
-            val myIntent = Intent(mContext, ViewPhotoActivity::class.java)
-            startActivity(myIntent)
-        }
-
     }
 
     override fun setValues() {
+
     }
+
 }
