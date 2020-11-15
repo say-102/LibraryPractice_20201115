80897d68a37c2db4faf19e0f9ad438ee3ee1692e
say-102 <say70007@naver.com>
2020-11-15 14:11:09 +0900

    실제 전화 권한 확인 + Manifest에 CALL_PHONE 권한 추가

--- app/src/main/java/kr/co/tjoeun/librarypractice_20201115/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/librarypractice_20201115/MainActivity.kt
@@ -1,10 +1,14 @@
 package kr.co.tjoeun.librarypractice_20201115
 
+import android.Manifest
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
 import kr.co.tjoeun.lifecycle_20201114.BaseActivity
 
@@ -23,9 +27,41 @@ class MainActivity : BaseActivity() {
 
 //            ACTION_CALL 을 이용해서 바로 전화 연결 => 권한획득처리도 같이 진행
 
-            val myUri = Uri.parse("tel:010-8888-7777")
-            val myIntent = Intent(Intent.ACTION_CALL, myUri)
-            startActivity(myIntent)
+//            권한이있는지? 있을때 행동+없을때 행동 (가이드북) => 실제 권한 체크
+//            TedPermission 라이브러리 활용
+
+//            권한 상태에 따른 행동 요령 (가이드북-permissionListner) 작성
+
+//            object : 가이드북을 담기 위한 임시(익명) 클래스 => 인터페이스 활용
+//            : PermissionListener => object가 어떤 종류의 가이드북을 담는지?
+//            Granted : 승인됐을때 코드, Denied : 거부되었을때 코드
+
+            val pl = object : PermissionListener {
+                override fun onPermissionGranted() {
+//                    승인이되었다면? 실제 전화 연결
+                    val myUri = Uri.parse("tel:010-8888-7777")
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+//                    거부되었다면? 통화 연결 불가 토스트
+                    Toast.makeText(mContext, "권한이 거부되어 통화연결이 불가능합니다.", Toast.LENGTH_SHORT).show()
+
+                }
+
+            }
+
+
+//            가이드북을 들고 => 실제로 권한 체크 실행
+
+            TedPermission.with(mContext)
+                .setPermissionListener(pl)
+                .setDeniedMessage("전화 권한 거부")
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
+
 
         }
 
